c4116c8eaafdb55988c673f588014640b23073a2
mid-tp <74594323+mid-tp@users.noreply.github.com>
2025-03-21 12:20:36 +0100

    Fixed the primes and import with pe_helpers

--- cpp/pe10.cpp
+++ cpp/pe10.cpp
@@ -5,8 +5,13 @@ using namespace std;
 
 
 int main() {
-    int n = 123;
-    int n_rev = pe_helpers::reverse(n);  // Call the function
-    cout << n_rev << endl;
+    int limit = 2000000;
+    u_int64_t total = 0;
+
+    for (int i=0; i < limit; i++){
+        if (!pe_methods::check_prime(i)) continue;
+        total += i;
+    }
+    cout << total << endl;
     return 0;
 }
