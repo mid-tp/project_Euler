38cab4e895dfe9fd35adf869c19c87f9b843ca51
mid-tp <74594323+mid-tp@users.noreply.github.com>
2025-03-22 20:52:06 +0100

    Additional stop criterion
    Added another stopping criterion to speed up the recursion. It can prune whenever we hit a chain of ones in the tree.

--- cpp/pe31.cpp
+++ cpp/pe31.cpp
@@ -1,13 +1,16 @@
 #include "pe_helpers.h"
 
 int count_ways(int res, int coin_index);
+
 // This is gonna be with recursion.
 
-vector<int> COINS = {1, 2, 5, 10, 20, 50, 100, 200};
+// vector<int> COINS = {1, 2, 5, 10, 20, 50, 100, 200};
+vector<int> COINS = {200, 100, 50, 20, 10, 5, 2, 1};
+
 
 int main(){
     cout << endl;
-    int count = count_ways(200, 0);
+    int count = count_ways(500, 0);
     cout << "We can make 200 in " << count << " different ways" << endl;
     cout << endl;
     return 0;
@@ -15,9 +18,10 @@ int main(){
 
 
 int count_ways(int res, int coin_index){
+    if (coin_index == COINS.size() - 1) return 1;
     if (res < 0) return 0;
     if (res == 0) return 1;
-    int num_times, coin;
+    int coin;
     int total = 0;
     for (int c=coin_index; c < COINS.size(); c++){
         // Check how often the current coin fits 
@@ -26,5 +30,4 @@ int count_ways(int res, int coin_index){
     }
     return total;
 }
-
 
